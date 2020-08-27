f33d686c4dbef26343f280aafec7c6c7e0c82d51
Theodore Butts <theodorebutts@gmail.com>
2020-08-26 22:49:36 -0600

    finished 18-1

---
+++ models/Pizza.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require('mongoose')
+
+const PIzzaSchema = new Schema({
+    pizzaName: {
+        type: String
+    },
+    createdBy: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    size: {
+        type: String,
+        default: 'Large'
+    },
+    toppings: []
+})
+
+const Pizza = model('Pizza', PIzzaSchema)
+
+module.exports = Pizza
