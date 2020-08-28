aaf8e71173d14df25ce2ef6b7fd41a7f7ce414d5
Theodore Butts <theodorebutts@gmail.com>
2020-08-27 20:33:02 -0600

    finished 18-2

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,26 +4,30 @@ const { db } = require('../models/Pizza')
 const pizzaController = {
     getAllPizza(req, res) {
         Pizza.find({})
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 })
             .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => {
-                console.log(err)
-                res.status(500).json(err)
-            })
+                console.log(err);
+                res.sendStatus(400);
+            });
     },
-
     getPizzaById({ params }, res) {
         Pizza.findOne({ _id: params.id })
-            .then(dbPizzaData => {
-                if (!dbPizzaData) {
-                    res.status(404).json({ message: 'No pizza found with this id!' })
-                    return
-                }
-                res.json(dbPizzaData)
+            .populate({
+                path: 'comments',
+                select: '-__v'
             })
+            .select('-__v')
+            .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => {
-                console.log(err)
-                res.status(400).json(err)
-            })
+                console.log(err);
+                res.sendStatus(400);
+            });
     },
 
     createPizza({ body }, res) {
@@ -31,7 +35,6 @@ const pizzaController = {
             .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => res.status(400).json(err))
     },
-
     updatePizza({ params, body }, res) {
         Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
             .then(dbPizzaData => {
@@ -43,7 +46,6 @@ const pizzaController = {
             })
             .catch(err => res.status(400).json(err));
     },
-
     deletePizza({ params }, res) {
         Pizza.findOneAndDelete({ _id: params.id })
             .then(dbPizzaData => {
--- models/Pizza.js
+++ models/Pizza.js
@@ -1,23 +1,44 @@
 const { Schema, model } = require('mongoose')
+const moment = require('moment')
 
-const PIzzaSchema = new Schema({
-    pizzaName: {
-        type: String
+const PizzaSchema = new Schema(
+    {
+        pizzaName: {
+            type: String
+        },
+        createdBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        },
+        size: {
+            type: String,
+            default: 'Large'
+        },
+        toppings: [],
+        comments: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Comment'
+            }
+        ]
     },
-    createdBy: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    size: {
-        type: String,
-        default: 'Large'
-    },
-    toppings: []
-})
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
 
-const Pizza = model('Pizza', PIzzaSchema)
+PizzaSchema.virtual('commentCount').get(function () {
+    return this.comments.length;
+});
+
+const Pizza = model('Pizza', PizzaSchema)
 
 module.exports = Pizza
