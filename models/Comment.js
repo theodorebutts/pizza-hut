7e1a31a5cf8a3bc7067698b8bd28a8baba446b29
Theodore Butts <theodorebutts@gmail.com>
2020-08-29 21:04:31 -0600

    finished 18.3

--- models/Comment.js
+++ models/Comment.js
@@ -1,18 +1,61 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment')
 
-const CommentSchema = new Schema({
-    writtenBy: {
-        type: String
+const ReplySchema = new Schema(
+    {
+        replyId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        replyBody: {
+            type: String
+        },
+        writtenBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        }
     },
-    commentBody: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
+    {
+        toJSON: {
+            getters: true
+        }
     }
-});
+
+)
+
+const CommentSchema = new Schema(
+    {
+        writtenBy: {
+            type: String
+        },
+        commentBody: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        },
+        replies: [ReplySchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.replies.length
+})
 
 const Comment = model('Comment', CommentSchema);
+const Reply = model('Reply', ReplySchema)
 
-module.exports = Comment;
+module.exports = { Comment, Reply };
